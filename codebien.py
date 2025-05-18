32cad1f69a920d5f59bf952d7be7ba7edf172bc2
feaustaus <fausto.nfjs@eduge.ch>
2025-05-18 19:24:59 +0200

    Update codebien.py

--- codebien.py
+++ codebien.py
@@ -1,74 +1,79 @@
+''' Auteur(s)   : Zubair Commar, Fausto Neefjes
+    Date        : 18 mai 2025
+    Version     : finale
+    Description : Projet de fin d'année pour faire sortir deux microbits l'un après l'autre d'un labyrinthe
+
+'''
+
+# Importations
 from maqueen import *
 from microbit import *
 from gripper import*
 
+
+# CONSTANTES
 WHITE = 1
 BLACK = 0
 MOTOR_FORWARD = 0
 MOTOR_BACKWARD = 1
 SPEED = 38
-DIRECTION_LEFT="left"
-DIRECTION_RIGHT = "right"
 SPEED_SLOW = 15
-STOP = 0
-DIRECTION_LEFT = "left"
-DIRECTION_RIGHT = "right"
-DIRECTION_FORWARD = "forward"
-DIRECTION_BACKWARD = "backward"
-dire= "horaire"
-Go = 500
+CONST = False
+ultra = 100
 
 
 
-'''L1=0
- M=1
- R1=2
- L2=3
- R2=4'''
-
+# fonction qui permet au maqueen de sortir du labyrinthe en collant le mur gauche
 def parcours_labyrinthe(speed) :
     if line_sensor(0)==1:
         motor_run(Motor.LEFT,speed,Direction.FORWARD)
     if line_sensor(1)== 1:
             motor_run(Motor.LEFT,speed,Direction.FORWARD)
             motor_run(Motor.RIGHT,speed,Direction.BACKWARD)
-    elif line_sensor(1) == 0:
+    elif line_sensor(1) == 0 and line_sensor(0)==1:
             motor_run(Motor.LEFT,speed,Direction.FORWARD)
             motor_run(Motor.RIGHT,speed,Direction.FORWARD)
     elif line_sensor(0)== 0:
             motor_run(Motor.LEFT,speed,Direction.BACKWARD)
             motor_run(Motor.RIGHT,speed,Direction.FORWARD)
-def pickup():
+            
+# fonction expérimentale qui essaye d'attrapper le microbit du deuxième microbit           
+def manoeuvre():
+    sleep(1500)
+    motor_run(Motor.LEFT,0,Direction.FORWARD)
+    motor_run(Motor.RIGHT,0,Direction.FORWARD)
+    sleep(1000)
     gripper(Servomotor.SERVO_P1, 0.8)
-    sleep(5000)
+    sleep(1000)
+    motor_run(Motor.LEFT,42,Direction.FORWARD)
+    motor_run(Motor.RIGHT,8,Direction.BACKWARD)
+    sleep(3700)
+    motor_run(Motor.LEFT,20,Direction.FORWARD)
+    motor_run(Motor.RIGHT,20,Direction.FORWARD)
+    sleep(500)
     gripper(Servomotor.SERVO_P1, 1)
-    sleep(10000)
-while True:
-#     parcours_labyrinthe(SPEED)
+    sleep(500)
+    motor_run(Motor.LEFT,0,Direction.BACKWARD)
+    motor_run(Motor.RIGHT,0,Direction.BACKWARD)
+    sleep(3000)
+    motor_run(Motor.LEFT,38,Direction.BACKWARD)
+    motor_run(Motor.RIGHT,38,Direction.BACKWARD)
+    sleep(2000)
+    motor_run(Motor.LEFT,0,Direction.FORWARD)
+    motor_run(Motor.RIGHT,0,Direction.FORWARD)
+    sleep(5000)
 
-    motor_run(Motor.LEFT,38,Direction.FORWARD)
-    motor_run(Motor.RIGHT,38,Direction.FORWARD)
-#     pickup()
-    print(ultrasonic())
-    if ultrasonic() < 27:
-        sleep(1500)
-        motor_run(Motor.LEFT,0,Direction.FORWARD)
-        motor_run(Motor.RIGHT,0,Direction.FORWARD)
-        sleep(1000)
-        gripper(Servomotor.SERVO_P1, 0.8)
-        motor_run(Motor.LEFT,50,Direction.FORWARD)
-        motor_run(Motor.RIGHT,8,Direction.BACKWARD)
-        sleep(3300)
-        motor_run(Motor.LEFT,20,Direction.FORWARD)
-        motor_run(Motor.RIGHT,20,Direction.FORWARD)
-        sleep(1000)
-        gripper(Servomotor.SERVO_P1, 1)
-        motor_run(Motor.LEFT,0,Direction.FORWARD)
-        motor_run(Motor.RIGHT,0,Direction.FORWARD)
-        sleep(3000)
-        motor_run(Motor.LEFT,38,Direction.BACKWARD)
-        motor_run(Motor.RIGHT,38,Direction.BACKWARD)
-        sleep(2000)
-        motor_run(Motor.LEFT,0,Direction.FORWARD)
-        motor_run(Motor.RIGHT,0,Direction.FORWARD)
-        sleep(5000)
+################################## Programme principal #########################
+while True:
+# permet de sortir le premier maqueen
+    parcours_labyrinthe(SPEED)
+# Si nécessaire, permet d'enclencher la manoeuvre    
+    if button_a.get_presses():
+        CONST = True 
+    ultra = ultrasonic()
+    print(ultra)
+    if ultra < 27 and ultra >22 :
+        if CONST == True:
+            manoeuvre()
+
+
